feaceef902bb96c07b89c57d6301cfbdf381cc56
Pandakil1er <Anshajsingh61@gmail.com>
2024-04-01 23:08:02 +0530

    can write files that are already writter and save them also

--- src/editor.rs
+++ src/editor.rs
@@ -53,7 +53,7 @@ impl Editor {
     }
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
-        let mut initial_status = String::from("help : Ctrl-Q = Quit");
+        let mut initial_status = String::from("help : Ctrl-S = Save | Ctrl-Q = Quit");
         let document = if args.len() > 1 {
             let file_name = &args[1];
             let doc = Document::open(&file_name);
@@ -97,6 +97,25 @@ impl Editor {
         let pressed_key = Terminal::read_key()?;
         match pressed_key {
             Key::Ctrl('q') => self.should_quit = true,
+            Key::Ctrl('s') => {
+                if self.document.save().is_ok() {
+                    self.status_message =
+                        StatusMessage::from("File Saved Successfully".to_string());
+                } else {
+                    self.status_message = StatusMessage::from("Error Writing File".to_string());
+                }
+            }
+            Key::Char(c) => {
+                self.document.insert(&self.cursor_position, c);
+                self.move_cursor(Key::Right);
+            }
+            Key::Delete => self.document.delete(&self.cursor_position),
+            Key::Backspace => {
+                if self.cursor_position.x > 0 || self.cursor_position.y > 0 {
+                    self.move_cursor(Key::Left);
+                    self.document.delete(&self.cursor_position);
+                }
+            }
             Key::Up
             | Key::Down
             | Key::Left
